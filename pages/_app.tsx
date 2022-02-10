84b0c3281fd5df8ad520dbf34e4f9694e211c73e
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2022-02-10 18:41:40 +0900

    add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
 
 import '../assets/app.css'
 
@@ -9,6 +10,21 @@ const App = ({ Component, pageProps }: AppProps) => (
       <meta name="robots" content="noindex, nofollow" />
     </Head>
     <Component {...pageProps} />
+
+    {/* Global site tag (gtag.js) - Google Analytics  */}
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-689WM9FP69"
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+    </Script>
   </>
 )
 
